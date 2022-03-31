cfd8686d4fa4c4c10383501326c2ad6328a1c65d
Adwaith R Krishna <adwaithrk19@gmail.com>
2022-03-31 09:11:35 +0530

    cycle detection program completed

--- Linked List/check_cycles.c
+++ Linked List/check_cycles.c
@@ -17,12 +17,42 @@ struct node *create_node(int data, struct node *previous)
     return link;
 }
 
+void traverse(struct node *traverse)
+{
+    while (traverse != NULL)
+    {
+        printf("%d\t", traverse->data);
+        traverse = traverse->next;
+    }
+    printf("\n");
+};
+
+void cycles_linked_list(struct node *first)
+{
+    struct node *tortoise = first;
+    struct node *hare = first;
+    while (hare->next != NULL)
+    {
+        hare = (hare->next)->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise)
+        {
+            printf("cycle detected!!!");
+            break;
+        }
+    }
+}
+
 int main()
 {
-    struct node *first = create_node(9, NULL);
-    struct node *second = create_node(10, first);
-    struct node *third = create_node(11, second);
-    struct node *fourth = create_node(12, third);
-    struct node *fifth = create_node(13, fourth);
-    struct node *sixth =
+    struct node *last = create_node(9, NULL);
+    struct node *second_last = create_node(10, last);
+    struct node *third_last = create_node(11, second_last);
+    struct node *fourth_last = create_node(12, third_last);
+    struct node *fifth_last = create_node(13, fourth_last);
+    struct node *sixth_last = create_node(14, fifth_last);
+    struct node *seventh_last = create_node(15, sixth_last);
+    last->next = sixth_last;
+
+    cycles_linked_list(seventh_last);
 }
