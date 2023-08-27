dd5459c26634a8076480dd12a9d792e63131c28d
adwaithrk <adwaithrk19@gmail.com>
2023-08-27 16:51:20 +0530

    lcs started

---
+++ LCS/lcs.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int LCS(string A, string B, int sizeA, int sizeB){
+
+    if(sizeA == 0 || sizeB == 0) return 0;
+
+    
+
+
+
+}
+
+
+
+
+
+
+int main(int argc, const char** argv) {
+
+
+
+    return 0;
+}
